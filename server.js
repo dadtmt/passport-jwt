3ec2f97bdcb26836a04c85bc012efd60023d1f4d
tmtm <thomas.culdaut@gmail.com>
2018-04-23 13:50:20 +0200

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const html = `
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <title>NodeJS server</title>
+    <link rel="stylesheet" href="/bootstrap.min.css" />
+  </head>
+  <body>
+    <h1>Auth with passport and JWT</h1>
+  </body>
+</html>`
+
+app.get('*', (req, res) => {
+  res.send(html)
+  res.end()
+})
+
+app.listen(8000)
